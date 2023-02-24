9a0139226b2b79f1929cf804c3d93166bcdba27e
Matthew Henderson <48498205+Phorbox@users.noreply.github.com>
2023-02-24 14:03:00 -0600

    Create makeSchema.php

---
+++ src/makeSchema.php
@@ -0,0 +1,26 @@
+<?php
+$table['raw'] = "CREATE TABLE `main`.`equipment_source`(
+    `type` VARCHAR(32) NOT NULL ,
+    `brand` VARCHAR(32) NOT NULL ,
+    `serial` VARCHAR(256) NOT NULL ) 
+    ENGINE = InnoDB;";
+
+$table['prod'] = "CREATE TABLE `main`.`equipment_production`(
+    `id` INT NOT NULL AUTO_INCREMENT ,
+    `type` INT NOT NULL ,
+    `brand` INT NOT NULL ,
+    `serial` VARCHAR(256) NOT NULL ,
+    PRIMARY KEY (`id`)) 
+    ENGINE = InnoDB;";
+
+$table['brands'] = "CREATE TABLE `main`.`brands`(
+    `id` INT NOT NULL AUTO_INCREMENT ,
+    `name` VARCHAR(32) NOT NULL ,
+    PRIMARY KEY (`id`)) 
+    ENGINE = InnoDB;";
+
+$table['types'] = "CREATE TABLE `main`.`types`(
+    `id` INT NOT NULL AUTO_INCREMENT ,
+    `name` INT NOT NULL ,
+    PRIMARY KEY (`id`)) 
+    ENGINE = InnoDB;";
