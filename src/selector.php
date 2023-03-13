4e1e361336fb8df22dbaf90e623136cd31b3759b
Matthew Henderson <48498205+Phorbox@users.noreply.github.com>
2023-03-13 01:24:01 -0500

    before switch to onedrive

--- src/selector.php
+++ src/selector.php
@@ -1,33 +1,26 @@
 <?php
 include_once("timer.php");
 include_once("baseQueries.php");
-function import($fname)
-{
-    importN($fname,0);
-}
-function importN($fname,$rown)
+
+function selectN($fname,$rown,$columni,$wherei)
 {
     $dblink = db_connect("main");
     $table = "equipment_source";
 
-    $fp = fopen($fname, "r");
-    $count = 0;
     $TimeAllStart = tStart();
-    $time_start = tStart();
-
-    autoOff($dblink);
+    
     $lastCount = 0;
     $columni = ['type', 'brand', 'serial'];
-    $commitRows = 100;
+    $commitRows = 1000;
+
+    select($dblink,$table,$columni,$wherei);
+
     while (($row = fgetcsv($fp)) !== FALSE) {
-        if(($count < $rown) or (count($row) != 3)){
+        if((count($row) != 3)){
             $count++;
-            if($count % 1000 == 0){
-                echo "$fname row $count\n";
-            }
             continue;
         }
-        
+
         insertQueryCSV($dblink, $table, $columni, $row);
         $count++;
         if ($count % $commitRows == 0) {
@@ -40,6 +33,7 @@ function importN($fname,$rown)
             commit($dblink);
 
             echo("writing ".($count-$commitRows)." to ".($count)."\n");
+
         }
     }
 
